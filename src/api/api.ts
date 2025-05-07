fa4aea0c11bde25fd550205acb89ee52013fe6e4
unknown <lucasmendoncawf@gmail.com>
2025-05-07 17:24:47 -0300

    fix intl to work on Netlify

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,27 @@
 import axios from 'axios';
 
+import nextIntlConfig from '../../next-intl.config';
+
 const api = axios.create({
   baseURL: '/.netlify/functions/',
 });
 
+export function getLanguageFromClientCookies() {
+  const cookies: Record<string, string> = document.cookie
+    .split('; ')
+    .reduce((acc: Record<string, string>, cookie) => {
+      const [name, value] = cookie.split('=');
+      acc[name] = value;
+      return acc;
+    }, {});
+
+  return cookies['NEXT_LOCALE'] || nextIntlConfig.defaultLocale;
+}
+
 api.interceptors.request.use(
   (config) => {
-    const langFromUrl = window.location.pathname.split('/')[1];
-    const language = langFromUrl || 'en';
+    const langFromUrl = getLanguageFromClientCookies();
+    const language = langFromUrl || nextIntlConfig.defaultLocale;
     config.headers['Accept-Language'] = language;
 
     return config;
