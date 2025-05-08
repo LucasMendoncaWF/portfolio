406110d1cc2e0caf571b8a0ac7a7b92566f198d2
unknown <lucasmendoncawf@gmail.com>
2025-05-08 16:03:58 -0300

    remove unused files

--- next-intl.config.ts
+++ next-intl.config.ts
@@ -1,6 +1,13 @@
-const nextIntlConfig = {
+import type { LocalePrefix, LocalePrefixMode } from 'next-intl/routing';
+
+const nextIntlConfig: {
+  locales: string[];
+  defaultLocale: string;
+  localePrefix: LocalePrefix<string[], LocalePrefixMode>;
+} = {
   locales: ['en', 'pt'],
   defaultLocale: 'en',
+  localePrefix: 'never',
 };
 
 export default nextIntlConfig;
--- src/i18n/routing.ts
+++
@@ -1,5 +0,0 @@
-import { defineRouting } from 'next-intl/routing';
-
-import nextIntlConfig from '../../next-intl.config';
-
-export const routing = defineRouting(nextIntlConfig);
